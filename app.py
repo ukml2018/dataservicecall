d9e3749d7b574dc775a6077986925b9dd91e857d
ukml2018 <ukmlcal@gmail.com>
2024-05-23 22:01:56 +0530

    4th commit

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ server = 'ib-azure-sql.database.windows.net'
 database = 'ImperialBrands'
 username = 'sqladmin'
 password = 'Admin#123'
-driver = '{ODBC Driver 17 for SQL Server}'
+driver = 'ODBC Driver 17 for SQL Server'
 
 # API endpoint for executing the SQL query
 @app.route('/query', methods=['GET'])
 def execute_query():
     try:
         print("Testing 1")
-        query = 'Select sf.sales_force_person_name, sf.sales_force_person_id, sf.outlet_id,dt.sales_line_info, dt.area_code, dt.customer_address,ff.outlet_address, ff.customer_number, ff.area_code from sales_force_outlet_mapping_en sf, ff_dashboard_outlet_mapping_en ff, distribution_tracking_en dt where sf.outlet_id = ff.outlet_ID and sf.outlet_id = dt.outlet_ID and  sf.outlet_id =1001' 
+        query = 'Select sf.sales_force_person_name, sf.sales_force_person_id,sf.outlet_id,dt.sales_line_info, dt.area_code, dt.customer_address,ff.outlet_address, ff.customer_number, ff.area_code from dbo.sales_force_outlet_mapping_en sf, dbo.ff_dashboard_outlet_mapping_en ff, dbo.distribution_tracking_en dt where sf.outlet_id = ff.outlet_ID and sf.outlet_id = dt.outlet_ID and  sf.sales_force_person_id =10001 group by sf.outlet_id, sf.sales_force_person_id, sf.sales_force_person_name, dt.sales_line_info, dt.area_code, dt.customer_address,ff.outlet_address, ff.customer_number, ff.area_code' 
         # Establish the database connection
         conn = pyodbc.connect(
             f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
@@ -45,5 +45,5 @@ def execute_query():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=False,port=8080,use_reloader=False)
 
